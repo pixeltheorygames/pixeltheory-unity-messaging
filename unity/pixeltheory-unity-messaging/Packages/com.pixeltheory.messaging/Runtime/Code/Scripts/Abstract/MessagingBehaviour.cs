985a57da7e9605c0b3777d4a4fcfa8b27662936d
Ellis Talley <git@ellistalley.com>
2023-01-07 15:23:25 +0900

    Added another MessagingBehaviour that inherits from PixelBehaviour<TypeComponent>.

--- unity/pixeltheory-unity-messaging/Packages/com.pixeltheory.messaging/Runtime/Code/Scripts/Abstract/MessagingBehaviour.cs
+++ unity/pixeltheory-unity-messaging/Packages/com.pixeltheory.messaging/Runtime/Code/Scripts/Abstract/MessagingBehaviour.cs
@@ -4,7 +4,56 @@ using UnityEngine;
 
 namespace Pixeltheory.Messaging
 {
-     public class MessagingBehaviour<TypeComponent, TypeRuntimeData> : PixelBehaviour<TypeComponent, TypeRuntimeData>, IMessageReceiver
+    public class MessagingBehaviour<TypeComponent> : PixelBehaviour<TypeComponent>, IMessageReceiver
+        where TypeComponent : PixelBehaviour<TypeComponent>
+    {
+        #region Fields
+        #region Inspector
+        [Header("MessagingBehaviour")]
+        [SerializeField] protected MessagingManager messagingManager;
+        #endregion //Inspector
+        #endregion //Fields
+        
+        #region Methods
+        #region Unity Messages
+        protected override void Awake()
+        {
+            base.Awake();
+            if (!base.isBeingDestroyed)
+            {
+                this.messagingManager = 
+                    this.messagingManager == null ? GameObject.FindObjectOfType<MessagingManager>() : this.messagingManager;
+                if (this.messagingManager != null)
+                {
+                    this.messagingManager.RegisterForMessages(this);
+                }
+                else
+                {
+                    Logging.Warn("[{0}] Couldn't find MessagingManager; did not register for messages.", this.name);
+                }
+            }
+        }
+
+        protected override void OnDestroy()
+        {
+            if (this.messagingManager != null)
+            {
+                this.messagingManager.DeregisterForMessages(this);   
+            }
+            base.OnDestroy();
+        }
+        #endregion //Unity Messages
+
+        #region IMessageReceiver
+        public string GameObjectName()
+        {
+            return this.name;
+        }
+        #endregion
+        #endregion //Methods
+    }
+
+    public class MessagingBehaviour<TypeComponent, TypeRuntimeData> : PixelBehaviour<TypeComponent, TypeRuntimeData>, IMessageReceiver
          where TypeComponent : PixelBehaviour<TypeComponent>
          where TypeRuntimeData : PixelRuntimeData<TypeRuntimeData>
      {
