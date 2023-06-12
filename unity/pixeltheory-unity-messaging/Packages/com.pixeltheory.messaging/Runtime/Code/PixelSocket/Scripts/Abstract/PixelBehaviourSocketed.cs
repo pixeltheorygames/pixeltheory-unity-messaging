97621c4b12f6615339b26a092715d05881531ed1
Ellis Talley <git@ellistalley.com>
2023-06-12 12:29:58 +0900

    Added namespaces to PixelSocket.cs and PixelBehaviourSocketed.cs.

--- unity/pixeltheory-unity-messaging/Packages/com.pixeltheory.messaging/Runtime/Code/PixelSocket/Scripts/Abstract/PixelBehaviourSocketed.cs
+++ unity/pixeltheory-unity-messaging/Packages/com.pixeltheory.messaging/Runtime/Code/PixelSocket/Scripts/Abstract/PixelBehaviourSocketed.cs
@@ -1,21 +1,23 @@
-using Pixeltheory;
 using UnityEngine;
 
 
-public abstract class PixelBehaviourSocketed<TypeSelf, TypeData> : PixelBehaviour<TypeSelf, TypeData>
-    where TypeSelf : PixelBehaviourSocketed<TypeSelf, TypeData>
-    where TypeData : PixelObject
+namespace Pixeltheory.Messaging
 {
-    #region Fields
-    #region Inspector
-    [Header("PixelBehaviourSocketed")]
-    [SerializeField] private GameObject prefabRootGameObject;
-    #endregion //Inspector
-    #endregion //Fields
+    public abstract class PixelBehaviourSocketed<TypeSelf, TypeData> : PixelBehaviour<TypeSelf, TypeData>
+        where TypeSelf : PixelBehaviourSocketed<TypeSelf, TypeData>
+        where TypeData : PixelObject
+    {
+        #region Fields
+        #region Inspector
+        [Header("PixelBehaviourSocketed")]
+        [SerializeField] private GameObject prefabRootGameObject;
+        #endregion //Inspector
+        #endregion //Fields
 
-    #region Properties
-    #region Protected
-    protected int UniqueSocketChannel => this.prefabRootGameObject?.GetInstanceID() ?? this.gameObject.GetInstanceID();
-    #endregion //Protected
-    #endregion //Properties
+        #region Properties
+        #region Protected
+        protected int UniqueSocketChannel => this.prefabRootGameObject?.GetInstanceID() ?? this.gameObject.GetInstanceID();
+        #endregion //Protected
+        #endregion //Properties
+    }
 }
--- unity/pixeltheory-unity-messaging/Packages/com.pixeltheory.messaging/Runtime/Code/PixelSocket/Scripts/Abstract/PixelSocket.cs
+++ unity/pixeltheory-unity-messaging/Packages/com.pixeltheory.messaging/Runtime/Code/PixelSocket/Scripts/Abstract/PixelSocket.cs
@@ -2,88 +2,92 @@ using System.Collections.Generic;
 using Pixeltheory;
 using UnityEngine.Pool;
 
-public abstract class PixelSocket<TypeInterface> : PixelObject
+
+namespace Pixeltheory.Messaging
 {
-   #region Fields
-   #region Private
-   private Dictionary<int, Dictionary<int, TypeInterface>> channelToIDAndListenersMap
-      = new Dictionary<int, Dictionary<int, TypeInterface>>();
-   private Dictionary<int, Dictionary<int, TypeInterface>.ValueCollection> channelToListenersCollectionMap
-      = new Dictionary<int, Dictionary<int, TypeInterface>.ValueCollection>();
-   #endregion //Private
-   #endregion //Fields
-
-   #region Methods
-   #region Public
-   public void Bind(TypeInterface listener, int channel)
+   public abstract class PixelSocket<TypeInterface> : PixelObject
    {
-      Dictionary<int, TypeInterface> idAndListeners;
-      if (this.channelToIDAndListenersMap.TryGetValue(channel, out idAndListeners))
-      {
-         idAndListeners.Add(listener.GetHashCode(), listener);
-      }
-      else
-      {
-         idAndListeners = DictionaryPool<int, TypeInterface>.Get();
-         idAndListeners.Add(listener.GetHashCode(), listener);
-         this.channelToIDAndListenersMap.Add(channel, idAndListeners);
-         this.channelToListenersCollectionMap.Add(channel, idAndListeners.Values);
-      }
-   }
+      #region Fields
+      #region Private
+      private Dictionary<int, Dictionary<int, TypeInterface>> channelToIDAndListenersMap
+         = new Dictionary<int, Dictionary<int, TypeInterface>>();
+      private Dictionary<int, Dictionary<int, TypeInterface>.ValueCollection> channelToListenersCollectionMap
+         = new Dictionary<int, Dictionary<int, TypeInterface>.ValueCollection>();
+      #endregion //Private
+      #endregion //Fields
 
-   public void Unbind(TypeInterface listener, int channel)
-   {
-      Dictionary<int, TypeInterface> idAndListeners;
-      if (this.channelToIDAndListenersMap.TryGetValue(channel, out idAndListeners))
+      #region Methods
+      #region Public
+      public void Bind(TypeInterface listener, int channel)
       {
-         idAndListeners.Remove(listener.GetHashCode());
-         if (idAndListeners.Count == 0)
+         Dictionary<int, TypeInterface> idAndListeners;
+         if (this.channelToIDAndListenersMap.TryGetValue(channel, out idAndListeners))
          {
-            this.channelToIDAndListenersMap.Remove(channel);
-            this.channelToListenersCollectionMap.Remove(channel);
-            DictionaryPool<int, TypeInterface>.Release(idAndListeners);
+            idAndListeners.Add(listener.GetHashCode(), listener);
+         }
+         else
+         {
+            idAndListeners = DictionaryPool<int, TypeInterface>.Get();
+            idAndListeners.Add(listener.GetHashCode(), listener);
+            this.channelToIDAndListenersMap.Add(channel, idAndListeners);
+            this.channelToListenersCollectionMap.Add(channel, idAndListeners.Values);
          }
       }
-      else
+
+      public void Unbind(TypeInterface listener, int channel)
       {
-         // If we get here, we are trying to unbind a listener that never bound itself.
-         // Something has gone wrong. We can use this opportunity to iterate through all
-         // channels and remove any empty listener lists. By doing so, hopefully we get
-         // the state of the channels and listeners back in sync with what is expected.
-         List<int> channelsToRemove = ListPool<int>.Get();
-         foreach (KeyValuePair<int, Dictionary<int, TypeInterface>> channelListenersPair in this.channelToIDAndListenersMap)
+         Dictionary<int, TypeInterface> idAndListeners;
+         if (this.channelToIDAndListenersMap.TryGetValue(channel, out idAndListeners))
          {
-            if (channelListenersPair.Value.Count == 0)
+            idAndListeners.Remove(listener.GetHashCode());
+            if (idAndListeners.Count == 0)
             {
-               channelsToRemove.Add(channelListenersPair.Key);
+               this.channelToIDAndListenersMap.Remove(channel);
+               this.channelToListenersCollectionMap.Remove(channel);
+               DictionaryPool<int, TypeInterface>.Release(idAndListeners);
             }
          }
-         for (int i = 0; i < channelsToRemove.Count; i++)
+         else
          {
-            int emptyChannel = channelsToRemove[i];
-            Dictionary<int, TypeInterface> emptyListeners = this.channelToIDAndListenersMap[emptyChannel];
-            this.channelToIDAndListenersMap.Remove(emptyChannel);
-            this.channelToListenersCollectionMap.Remove(emptyChannel);
-            DictionaryPool<int, TypeInterface>.Release(emptyListeners);
+            // If we get here, we are trying to unbind a listener that never bound itself.
+            // Something has gone wrong. We can use this opportunity to iterate through all
+            // channels and remove any empty listener lists. By doing so, hopefully we get
+            // the state of the channels and listeners back in sync with what is expected.
+            List<int> channelsToRemove = ListPool<int>.Get();
+            foreach (KeyValuePair<int, Dictionary<int, TypeInterface>> channelListenersPair in this.channelToIDAndListenersMap)
+            {
+               if (channelListenersPair.Value.Count == 0)
+               {
+                  channelsToRemove.Add(channelListenersPair.Key);
+               }
+            }
+            for (int i = 0; i < channelsToRemove.Count; i++)
+            {
+               int emptyChannel = channelsToRemove[i];
+               Dictionary<int, TypeInterface> emptyListeners = this.channelToIDAndListenersMap[emptyChannel];
+               this.channelToIDAndListenersMap.Remove(emptyChannel);
+               this.channelToListenersCollectionMap.Remove(emptyChannel);
+               DictionaryPool<int, TypeInterface>.Release(emptyListeners);
+            }
+            ListPool<int>.Release(channelsToRemove);
          }
-         ListPool<int>.Release(channelsToRemove);
       }
-   }
-   #endregion //Public
+      #endregion //Public
 
-   #region Protected
-   protected Dictionary<int, TypeInterface>.ValueCollection GetListenersCollection(int channel)
-   {
-      Dictionary<int, TypeInterface>.ValueCollection listeners;
-      if (!this.channelToListenersCollectionMap.TryGetValue(channel, out listeners))
+      #region Protected
+      protected Dictionary<int, TypeInterface>.ValueCollection GetListenersCollection(int channel)
       {
-         Dictionary<int, TypeInterface> idToListenersMap = DictionaryPool<int, TypeInterface>.Get();
-         this.channelToIDAndListenersMap.Add(channel, idToListenersMap);
-         this.channelToListenersCollectionMap.Add(channel, idToListenersMap.Values);
-         listeners = idToListenersMap.Values;
+         Dictionary<int, TypeInterface>.ValueCollection listeners;
+         if (!this.channelToListenersCollectionMap.TryGetValue(channel, out listeners))
+         {
+            Dictionary<int, TypeInterface> idToListenersMap = DictionaryPool<int, TypeInterface>.Get();
+            this.channelToIDAndListenersMap.Add(channel, idToListenersMap);
+            this.channelToListenersCollectionMap.Add(channel, idToListenersMap.Values);
+            listeners = idToListenersMap.Values;
+         }
+         return listeners;
       }
-      return listeners;
+      #endregion //Protected
+      #endregion //Method
    }
-   #endregion //Protected
-   #endregion //Method
 }
